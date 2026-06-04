7beda50b973fcd9e3af53508d2134da8fe608c1a
keshvapatel <Keshva2004@gmail.com>
2026-06-04 15:09:48 +0530

    Configure files again..

--- frontend/src/Context/FlashProvider.jsx
+++ frontend/src/Context/FlashProvider.jsx
@@ -1,17 +1,16 @@
 // src/context/FlashProvider.jsx
 import React, { createContext, useContext, useState } from "react";
-import Flash from "../Components/HeroSection/flash"; // Correct path & casing
+import Flash from "../Components/HeroSection/Flash"; // Correct path & casing
 
 const FlashContext = createContext();
 export const useFlash = () => useContext(FlashContext);
 
-export  const FlashProvider = ({ children }) => {
+export const FlashProvider = ({ children }) => {
   const [flashMessage, setFlashMessage] = useState("");
   const [flashSeverity, setFlashSeverity] = useState("info");
   const [open, setOpen] = useState(false);
 
   const showFlash = (message, severity = "info") => {
- 
     setFlashMessage(message);
     setFlashSeverity(severity);
     setOpen(true);
